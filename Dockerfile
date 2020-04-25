cf31310f90fd6e17c5509553d1b72aa603966982
lukoshkin <lukoshkin@phystech.edu>
2020-04-25 14:33:56 +0300

    Write command-line interface

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,5 @@
 FROM quay.io/fenicsproject/stable:latest
 
-# configure all my vim settings inside the container
-RUN apt-get update && apt-get -y install \
-    vim-gui-common \
-    clang-tools-9
-
-COPY .vimrc .inputrc /root/
-COPY .ycm_extra_conf.py /root/.vim/
-
-RUN git clone https://github.com/VundleVim/Vundle.vim.git \
-    /root/.vim/bundle/Vundle.vim && \
-    vim +PluginInstall +qall && \
-    python3 /root/.vim/bundle/YouCompleteMe/install.py --clang-completer
-
-RUN git clone https://github.com/kien/ctrlp.vim.git \
-    /root/.vim/bundle/ctrlp.vim
-
 # uninstall jupyter since it was installed
 # in root install directory in the base image
 RUN pip uninstall -y \
@@ -28,7 +12,24 @@ RUN pip uninstall -y \
                 nbconvert \
                 nbformat
 
+RUN apt-get update && apt-get -y install \
+    vim-gui-common \
+    clang-tools-9
+
 USER fenics
+
+# configure all my vim settings inside the container
+RUN mkdir /home/fenics/.vim
+COPY --chown=$USER .vimrc .inputrc /home/fenics/
+COPY --chown=$USER .ycm_extra_conf.py /home/fenics/.vim/
+
+RUN git clone https://github.com/VundleVim/Vundle.vim.git \
+    $HOME/.vim/bundle/Vundle.vim && \
+    vim +PluginInstall +qall && \
+    python3 $HOME/.vim/bundle/YouCompleteMe/install.py --clang-completer
+
+RUN git clone https://github.com/kien/ctrlp.vim.git \
+    $HOME/.vim/bundle/ctrlp.vim
 
 # install jupyter in HOME dir, update PATH, install jupyter nbextensions
 ENV PATH="/home/fenics/.local/bin:${PATH}"
@@ -41,8 +42,8 @@ RUN git clone https://github.com/lambdalisue/jupyter-vim-binding \
     jupyter nbextension enable vim_binding/vim_binding && \
     jupyter contrib nbextension install --user
 
-COPY custom.js /home/fenics/.jupyter/custom/
-COPY notebook.json /home/fenics/.jupyter/nbconfig/
+COPY --chown=$USER custom.js /home/fenics/.jupyter/custom/
+COPY --chown=$USER notebook.json /home/fenics/.jupyter/nbconfig/
 
 # Since CMD /sbin/my_init of the base image requires root privileges, change user
 USER root
--- runjnb.sh
+++ runjnb.sh
@@ -1,60 +1,122 @@
 #!/bin/bash
 # bad coding by V.Lukoshkin
 
-# Some simple asserts
-[ -z $1 ] || [ -z $2 ] && echo "Two arguments must be passed. Exiting." &&  exit 1
-[ -n "$(\grep -oE '[^0-9]*' <<< $2)" ] && echo "Invalid port. Exiting." && exit 1
+port='8237' # default port
+print_flag=true
+rm_flag=false
 
-image="$1"                # from which image to make a container.
-port="127.0.0.1:$2"       # host port (machine port)
+print_help_msg () {
+  echo -e "Usage: \t./runjnb.sh [OPTIONS] IMAGE [OPTIONS]"
+  echo -e "\nOptions:"
+  printf "\n  %-20s\t  Output this message" '-h, --help'
+  printf "\n  %-20s\t  Launch a NEW container from the image selected." '-0, --from-scratch'
+  printf "\n  %-20s\t  Delete the old one with the conflicting name if this is the case" ''
+  printf "\n  %-20s\t  Specify the machine's port to which map the one of container\n" '-p, --port <xxxx>'
+  exit
+}
 
-name="$(sed -E 's/([a-zA-Z0-9_.-]+):([a-zA-Z0-9_.-]*)/\1-\2/' <<< $1)"
-dir=/home/fenics          # working directory - directory inside the container
-                          # where commands will be executed
+set_port () {
+  port=$(tr -d "'" <<< $1)
+  [ "$(\grep -oE '[^0-9]*' <<< $port)" ] \
+    && echo "Error: Invalid port - $port" >&2 && exit 1
+  print_flag=false
+}
 
-warning="/ You are trying to run chrome as root. On the day this\n
-        script was written, it was only possible to do by adding\n 
-        '--no-sandbox' option to chrome's call in this script.\n 
-        For your safety, it is highly discouraged to run chrome\n 
-        without sandboxing, unless you know what you are doing /"
+set -- $(getopt -o h,p:,0 -l help,port:,from-scratch --name "$0" -- "$@")
 
-function ctrl_c() {
-  echo
-  $(docker inspect -f {{.State.Running}} $name 2> /dev/null) \
-    && echo "Stopping $name" \
-    && docker stop $name > /dev/null
-  exit 2
+while : 
+do
+  case "$1" in
+    -p|--port) set_port $2; shift 2;;
+    -h|--help) print_help_msg;;
+    -0|--from-scratch) rm_flag=true; shift;;
+    --) shift; break;;
+    *) echo "Error: Unknown option $1" >&2; exit 1;;
+  esac
+done
+
+(($# == 0)) && echo "No image specified" >&2 && exit 1
+image=$(tr -d "'" <<< $1)
+[ -z $(docker images -q $image) ] \
+  && echo "Error: Image does not exist" >&2 && exit 1
+name="$(sed -E 's/([a-zA-Z0-9_.-]+):([a-zA-Z0-9_.-]*)/\1-\2/' <<< $image)"
+
+exists="$(docker ps -af name=$name | \grep $name)"
+$rm_flag && [ "$exists" ] \
+  && echo "Removing container '$name'" \
+  && docker stop $name > /dev/null \
+  && docker rm $name > /dev/null \
+  && exists=''
+
+$print_flag && ! [ "$exists" ] \
+  && echo "No port set, using the default one, $port"
+! $print_flag && [ "$exists" ] \
+  && echo "Port option does not affect an existing container"
+port="127.0.0.1:$port"
+
+print_warning () {
+  echo -e "\nWARNING"
+  printf "\n%2s You are trying to run chrome as root. On the day this\n" ''
+  printf "%2s script was written, it was only possible to do by adding\n" ''
+  printf "%2s '--no-sandbox' option to chrome's call in this script.\n" ''
+  printf "%2s For your safety, it is highly discouraged to run chrome\n" ''
+  printf "%2s without sandboxing, unless you know what you are doing\n" ''
+  exit
 }
 
 # if one executes this script with sudo, then print warning and exit
-[ $EUID -eq 0 ] && echo -e $warning && ctrl_c;
+[ $EUID -eq 0 ] && print_warning
 
+# The comments below describe optins of `docker run` which follows them
+# ---------------------------------------------------------------------
 # --name to assign a name to the container
 # -w to set the working directory
 # -v dir1:dir2  -- this mounts host dir1 to remote dir2
 # -d to run container in the background and print container ID
 # -a to attach to STDIN, STDOUT or STDERR
 # -p to map ports: host to remote (that of container) 
+# ---------------------------------------------------------------------
 
-# If the container already exists, resume it. 
-# Otherwise create a new one and run jupyter notebook 
-# within the container
-
-[ "$(docker ps -af name=$name | grep $name)" ] \
-  && echo "Resuming container '$name'" \
+dir=/home/fenics
+[ "$exists" ] && echo "Resuming container '$name'" \
   && docker start $name > /dev/null \
-  || (echo "Creating container '$name'" 
+  || (echo "Creating container '$name'"
     docker run --name $name \
     -w $dir -v $PWD:$dir/shared \
     -d -p $port:8888 \
-    $image 'jupyter-notebook --ip=0.0.0.0' > /dev/null)
+    $image 'tail -f /dev/null' > /dev/null)
+# ---------------------------------------------------------------------
+docker exec --user="fenics" -d $name jupyter notebook --ip=0.0.0.0
 
-sleep 4 # if it does not work for you, increase the delay
-token=$(docker logs $name 2>&1 | grep -o "token=[a-z0-9]*" | sed -n 1p)
-google-chrome http://$port/?$token
+run_notebook () {
+  token=$(docker exec --user="fenics" $name jupyter notebook list |
+    tail -1 | \grep -o "token=[a-z0-9]*")
+  \google-chrome http://$port/?$token
+}
+
+run_bash () {
+  docker exec -ti $name bash -c 'su -- fenics'
+}
+
+function ctrl_c() {
+  echo
+  $(docker inspect -f {{.State.Running}} $name 2> /dev/null) \
+    && echo "Stopping container '$name'" \
+    && docker stop $name > /dev/null
+  exit
+}
 
 #execute ctrl_c function when Ctrl-C is pressed
 trap ctrl_c SIGINT
 
-# -f, --follow    Follow log output
-docker logs --since 40s -f $name
+echo -e "\nPress Ctrl-C to exit\n"
+while :
+do 
+  echo 'What do we do next? <bash|notebook>'
+  read -p '> ' cmd
+  case "$cmd" in 
+    bash) run_bash;;
+    notebook) run_notebook;;
+    *) echo -e "\nTry again";;
+  esac
+done
