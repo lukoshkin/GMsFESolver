affb09985e4fd8a6020d7841fa26f30d181ddb63
lukoshkin <lukoshkin@phystech.edu>
2020-05-06 01:11:48 +0300

    Extend cutils

--- src/cutils/pymodule2.cpp
+++ src/cutils/pymodule2.cpp
@@ -4,7 +4,9 @@
 #include <pybind11/stl.h>
 #include "EPDE.h"
 #include "GEP.h"
-#include "IF.h"
+#include "GC.h"
+#include "MIF.h"
+
 
 using namespace dolfin;
 namespace py = pybind11;
@@ -15,6 +17,7 @@ using matvec_pair = std::pair<
 using matmat_pair = std::pair<
     std::shared_ptr<Matrix>,
     std::shared_ptr<Matrix>>;
+
 
 matmat_pair
 unloaded_matrices(std::shared_ptr<Function>& k) {
@@ -69,73 +72,106 @@ assemble_Ab(std::shared_ptr<Function>& k) {
 }
 
 
-py::array_t<double>
-mass_integral_matrix(
+matvec_pair
+diagonal_coupling(
         std::shared_ptr<Function>& k,
-        std::vector<std::shared_ptr<Function>>& F1,
-        std::vector<std::shared_ptr<Function>>& F2) {
-
+        std::shared_ptr<Function>& xi,
+        std::shared_ptr<Function>& f) {
     auto V = k->function_space();
-    size_t N_EL(F1.size());
+    GC::BilinearForm a(V, V);
+    a.xi_1 = xi;
+    a.xi_2 = xi;
+    a.k = k;
 
-    IF::Form_M mass_form(V->mesh());
-    mass_form.set_coefficient(0, k);
+    GC::LinearForm L(V);
+    L.xi_1 = xi;
+    L.f = f;
+    
+    std::shared_ptr<Matrix> A(new Matrix);
+    std::shared_ptr<Vector> b(new Vector);
+    assemble(*A, a);
+    assemble(*b, L);
+    return std::make_pair(A, b);
+}
 
-    py::array_t<double> mass({N_EL, N_EL});
-    auto M_info = mass.request();
-    double * M_ptr = (double *)M_info.ptr;
+
+std::shared_ptr<Matrix>
+offdiagonal_coupling(
+        std::shared_ptr<Function>& k,
+        std::shared_ptr<Function>& xi_1,
+        std::shared_ptr<Function>& xi_2) {
+    auto V = k->function_space();
+    GC::BilinearForm a(V, V);
+    a.xi_1 = xi_1;
+    a.xi_2 = xi_2;
+    a.k = k;
+    
+    std::shared_ptr<Matrix> A(new Matrix);
+    assemble(*A, a);
+    return A;
+}
+
+std::vector<std::vector<double>>
+project_embed(
+        py::array_t<double>& Nv,
+        std::shared_ptr<Function>& xi,
+        std::shared_ptr<FunctionSpace>& W) {
+
+    auto V = xi->function_space();
+    auto v = std::make_shared<Function>(V);
+    auto e = std::make_shared<Function>(W);
+
+    auto Nv_info = Nv.request();
+    double * Nv_ptr = (double *)Nv_info.ptr;
+    size_t N_EL(Nv_info.shape[0]), DIM(V->dim());
+
+    std::vector<std::vector<double>> ms_dofs(
+            N_EL, std::vector<double>(W->dim()));
 
     for (uint i=0; i<N_EL; ++i) {
-        mass_form.set_coefficient(1, F1[i]);
-        for (uint j=i; j<N_EL; ++j) {
-            mass_form.set_coefficient(2, F2[j]);
-            M_ptr[i*N_EL+j] = assemble(mass_form);
-        }
+        v->vector()->set_local(
+                std::vector<double>(
+                    Nv_ptr+i*DIM, Nv_ptr+i*DIM+DIM));
+
+        *v->vector() *= *xi->vector();
+        LagrangeInterpolator::interpolate(*e, *v);
+        e->vector()->get_local(ms_dofs[i]);
     }
-    return mass;
+    return ms_dofs;
 }
 
 
 py::array_t<double>
 stiffness_integral_matrix(
         std::shared_ptr<Function>& k,
-        std::vector<std::shared_ptr<Function>>& F1,
-        std::vector<std::shared_ptr<Function>>& F2) {
+        std::vector<std::vector<double>>& Psi1,
+        std::vector<std::vector<double>>& Psi2,
+        std::shared_ptr<MeshFunction<size_t>>& markers) {
 
-    auto V = k->function_space();
-    size_t N_EL(F1.size());
+    auto W = k->function_space();
+    auto u = std::make_shared<Function>(W);
+    auto v = std::make_shared<Function>(W);
+    size_t N_EL(Psi1.size());
 
-    IF::Form_S stiffness_form(V->mesh());
+    MIF::Form_S stiffness_form(W->mesh());
     stiffness_form.set_coefficient(0, k);
+    stiffness_form.dx = markers;
 
     py::array_t<double> stiffness({N_EL, N_EL});
     auto S_info = stiffness.request();
     double * S_ptr = (double *)S_info.ptr;
 
     for (uint i=0; i<N_EL; ++i) {
-        stiffness_form.set_coefficient(1, F1[i]);
+        u->vector()->set_local(Psi1[i]);
+        stiffness_form.set_coefficient(1, u);
         for (uint j=i; j<N_EL; ++j) {
-            stiffness_form.set_coefficient(2, F2[j]);
+            v->vector()->set_local(Psi2[j]);
+            stiffness_form.set_coefficient(2, v);
             S_ptr[i*N_EL+j] = assemble(stiffness_form);
         }
     }
     return stiffness;
 }
-
-
-py::array_t<double>
-mass_integral_matrix(
-        std::shared_ptr<Function>& k,
-        std::vector<std::shared_ptr<Function>>& F) 
-{return mass_integral_matrix(k, F, F);}
-
-
-py::array_t<double>
-stiffness_integral_matrix(
-        std::shared_ptr<Function>& k,
-        std::vector<std::shared_ptr<Function>>& F)
-{return stiffness_integral_matrix(k, F, F);}
-
 
 PYBIND11_MODULE(SIGNATURE, m) {
     m.doc() = "Faster calculations with C++";
@@ -163,40 +199,27 @@ PYBIND11_MODULE(SIGNATURE, m) {
             py::arg("kappa"), py::arg("source"));
 
     m.def(
-            "mass_integral_matrix",
-            (py::array_t<double>(*)(
-                std::shared_ptr<Function>&,
-                std::vector<std::shared_ptr<Function>>&,
-                std::vector<std::shared_ptr<Function>>&)
-            ) &mass_integral_matrix,
-            "Compute generalized mass matrix <kappa Psi1_i, Psi2_j>",
-            py::arg("kappa"), py::arg("Psi1"), py::arg("Psi2"));
+            "diagonal_coupling",
+            &diagonal_coupling,
+            "Computes unloaded diagonal blocks",
+            py::arg("kappa"), py::arg("xi"), py::arg("RHS"));
 
     m.def(
-            "mass_integral_matrix",
-            (py::array_t<double>(*)(
-                std::shared_ptr<Function>&,
-                std::vector<std::shared_ptr<Function>>&)
-            ) &mass_integral_matrix,
-            "Assemble mass matrix <kappa Psi_i, Psi_j>",
-            py::arg("kappa"), py::arg("Psi"));
+            "offdiagonal_coupling",
+            &offdiagonal_coupling,
+            "Computes unloaded offdiagonal blocks",
+            py::arg("kappa"), py::arg("xi_1"), py::arg("xi_2"));
 
     m.def(
-            "stiffness_integral_matrix",
-            (py::array_t<double>(*)(
-                std::shared_ptr<Function>&,
-                std::vector<std::shared_ptr<Function>>&,
-                std::vector<std::shared_ptr<Function>>&)
-            ) &stiffness_integral_matrix,
-            "Compute generalized stiffness matrix <kappa grad(Psi1_i), grad(Psi2_j)>",
-            py::arg("kappa"), py::arg("Psi1"), py::arg("Psi2"));
+            "project_embed",
+            &project_embed,
+            "Extrapolates local ms functions to W with zeros",
+            py::arg("NodalValues"), py::arg("xi"), py::arg("W"));
 
     m.def(
             "stiffness_integral_matrix",
-            (py::array_t<double>(*)(
-                std::shared_ptr<Function>&,
-                std::vector<std::shared_ptr<Function>>&)
-            ) &stiffness_integral_matrix,
+            &stiffness_integral_matrix,
             "Assemble stiffness matrix <kappa grad(Psi_i), grad(Psi_j)>",
-            py::arg("kappa"), py::arg("Psi"));
+            py::arg("kappa"), py::arg("Psi_i"),
+            py::arg("Psi_j"), py::arg("markers"));
 }
