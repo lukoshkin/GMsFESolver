8fbe8b7028dee9a0b773f8502ebea716a0e9a078
lukoshkin <lukoshkin@phystech.edu>
2020-05-27 18:38:04 +0300

    Fix bug in P1case

--- src/p1case.py
+++ src/p1case.py
@@ -4,11 +4,32 @@
 # This is valid for FEniCS-2019.1.0.
 # There is no information about newer versions
 
+import math
 import numpy as np
 import scipy.linalg
 
 from ufl import *
 from dolfin import *
+
+
+def zero_extrapolate(Nv, V, W, j, i):
+    """
+    Many times faster (40-300x) than
+    equivalent operation with LagrangeInterpolator
+    """
+    N = W.dim()
+    n = int(math.sqrt(N))
+    I = np.arange(N).reshape(n, n)
+
+    p = int(math.sqrt(Nv.shape[-1])/2)
+    vertices = I[i*p:(i+2)*p+1, j*p:(j+2)*p+1].flatten()
+
+    v2d_V = vertex_to_dof_map(V)
+    v2d_W = vertex_to_dof_map(W)
+    Nv_W = np.zeros((len(Nv), N))
+
+    Nv_W[:, v2d_W[vertices]] = Nv[:, v2d_V]
+    return Nv_W
 
 
 def overlap_map(n):
@@ -56,26 +77,21 @@ class GMsFEUnit:
     n_blocks    number of coarse blocks along one dimension
     """
     def __init__(self, reg_id, n_el, n_blocks, cutils):
+        tol = 1./(n_el*n_blocks)
         subd = lambda x_l, x_r, y_l, y_r: (
             CompiledSubDomain(
                 '((x[0] >= x_l-tol) && (x[0] <= x_r+tol))'
                 '&& ((x[1] >= y_l-tol) && (x[1] <= y_r+tol))',
-                x_l=x_l, x_r=x_r, y_l=y_l, y_r=y_r, tol=1e-12));
+                x_l=x_l, x_r=x_r, y_l=y_l, y_r=y_r, tol=1e-8));
+        rel_id = np.array([0, 1, n_blocks-2, n_blocks-1, n_blocks])
 
         tau = 1./n_blocks
-        def middle_point(reg_id):
-            col = reg_id % (n_blocks-1)
-            row = reg_id // (n_blocks-1)
-            x_m, y_m = (col+1)*tau, (row+1)*tau
-            return np.column_stack([x_m, y_m])
-
-        rel_id = np.array([0, 1, n_blocks-2, n_blocks-1, n_blocks])
-        abs_id = reg_id + rel_id
-
-        self._midp = dict(zip(rel_id, middle_point(abs_id)))
-        x_m, y_m = self._midp[0]
+        col = reg_id % (n_blocks-1)
+        row = reg_id // (n_blocks-1)
+        x_m, y_m = (col+1)*tau, (row+1)*tau
         x_l, x_r = x_m - tau, x_m + tau
         y_l, y_r = y_m - tau, y_m + tau
+        self._midp = x_m, y_m
 
         self._subd = {}
         self._subd[rel_id[0]] = subd(x_l, x_r, y_l, y_r)
@@ -88,16 +104,15 @@ class GMsFEUnit:
         self._subd[-rel_id[1]] = subd(x_l, x_m, y_l, y_r)
         self._subd[-rel_id[2]] = subd(x_m, x_r, y_l, y_m)
         self._subd[-rel_id[3]] = subd(x_l, x_r, y_l, y_m)
-        self._subd[-rel_id[4]] = subd(x_m, x_r, y_l, y_m)
+        self._subd[-rel_id[4]] = subd(x_l, x_m, y_l, y_m)
 
         self._nblocks = n_blocks
         self._cutils = cutils
         self._tol = tau/n_el
-        self._nel = n_el
 
     def snapshotSpace(self, k):
         """
-        Nv - Nodal values
+        Returns: Nv - Nodal Values, 2D np.ndarray
         """
         A, b = self._cutils.assemble_Ab(k.cpp_object())
 
@@ -132,7 +147,21 @@ class GMsFEUnit:
         w, h = scipy.linalg.eigh(M, S, eigvals=which)
         return (h.T @ Nv), w
 
-    def partitionFunction(self, k, x_m, y_m):
+    def multiscaleFunctions(self, k, Nv):
+        """
+        Multiplies the dofs of online functions
+        by those of unity partition
+        """
+        V = k.function_space()
+        xi = self.partitionFunction(k)
+        return Nv * xi.vector().get_local()
+
+    def partitionFunction(self, k):
+        """
+        Returns partition unity of function
+        for a region specified by `k`
+        """
+        x_m, y_m = self._midp
         u_mv = Expression(
                 '1 - k*fabs(x[1]-y_m)',
                 k=self._nblocks, y_m=y_m, degree=1)
@@ -153,72 +182,36 @@ class GMsFEUnit:
         return xi
 
     def diagonalBlock(self, k, Nv, RHS):
-        xi = self.partitionFunction(k, *self._midp[0])
-
-        #A, b = self._cutils.diagonal_coupling(
-        #    k.cpp_object(), xi.cpp_object(), RHS.cpp_object())
-
+        """
+        Returns diagonal block `A_ii` and vector slice `b_i` in
+        the global matrix `A` and load vector `b`, repspectively
+        """
         A, b = self._cutils.assemble_Ab(
                 k.cpp_object(), RHS.cpp_object())
+
         A = Nv @ A.array() @ Nv.T
         b = Nv @ b.get_local()
         return A, b
 
     def offdiagonalBlock(self, k_i, Nv_i, Nv_j, pos):
         """
-        pos - overlap position of two adjacent regions
-              from the view point of the one with higher id
-
-        Returns A_ij
+        pos - position of j-th block relative to i-th block
+        Returns off-diagonal block `A_ij` in the global matrix `A`
         """
         V = k_i.function_space()
         subm_i = SubMesh(V.mesh(), self._subd[pos])
         subm_j = SubMesh(V.mesh(), self._subd[-pos])
 
-        mask_i = vertex_to_dof_map(V)[
-                subm_i.data().array('parent_vertex_indices', 0)]
-        mask_j = vertex_to_dof_map(V)[
-                subm_j.data().array('parent_vertex_indices', 0)]
+        v2d = vertex_to_dof_map(V)
+        mask_i = v2d[subm_i.data().array('parent_vertex_indices', 0)]
+        mask_j = v2d[subm_j.data().array('parent_vertex_indices', 0)]
 
         subV = FunctionSpace(subm_i, 'P', 1)
         k = Function(subV)
-        #k = project(k_i, subV)
 
-        #Nv_subi = []
-        #for v_dofs in Nv_i:
-        #    v = Function(V)
-        #    v.vector().set_local(v_dofs)
-        #    v = project(v, subV)
-        #    vn_dofs = v.vector().get_local()
-        #    Nv_subi.append(vn_dofs)
-        #Nv_subi = np.array(Nv_subi)
-
-        #mesh = UnitSquareMesh(2*self._nel, 2*self._nel)
-        #mesh.scale(2./self._nblocks)
-        #leftcorner = self._midp[pos] - 1./self._nblocks
-        #mesh.translate(Point(*leftcorner))
-        #Va = FunctionSpace(mesh, 'P', 1) 
-        ##return V, Va, subV
-
-        #Nv_subj = []
-        #for i, v_dofs in enumerate(Nv_j):
-        #    v = Function(Va)
-        #    v.vector().set_local(v_dofs)
-        #    v = project(v, subV)
-        #    vn_dofs = v.vector().get_local()
-        #    Nv_subj.append(vn_dofs)
-        #Nv_subj = np.array(Nv_subj)
-
-        k.vector()[vertex_to_dof_map(subV)] = k_i.vector()[mask_i]
         d2v = dof_to_vertex_map(subV)
+        k.vector()[:] = k_i.vector()[mask_i[d2v]]
 
-        xi_i = self.partitionFunction(k, *self._midp[0])
-        xi_j = self.partitionFunction(k, *self._midp[pos])
-
-        #A,_ = self._cutils.assemble_Ab(k.cpp_object())  #del
-        A = self._cutils.offdiagonal_coupling(
-                k.cpp_object(), xi_i.cpp_object(), xi_j.cpp_object())
-        #A = Nv_subi @ A.array() @ Nv_subj.T
-
-        A = Nv_i[:, mask_i][:, d2v] @ A.array() @ Nv_j.T[mask_j][d2v]
+        A,_ = self._cutils.assemble_Ab(k.cpp_object())
+        A = Nv_i[:, mask_i[d2v]] @ A.array() @ Nv_j.T[mask_j[d2v]]
         return A
